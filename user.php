12ecdcdd4351f4d9d8f609974911cdc54c2f418c
Roxan Norma Jimena Cruz Chete <201800230@colsanjose.edu.gt>
2020-04-23 13:58:42 -0600

    SI

---
+++ user.php
@@ -0,0 +1,28 @@
+<?php
+  $user=array("user01","user02","user03");
+  $pass=array(0912,0204,1901);
+   $estado=false; 
+
+   $nombre=%POST['Nombre'];
+   $password=$_POST['Pass'];
+
+   $tam=count($user); 
+
+   for ($x=0; $x<$tam;$x++)
+   {
+   if ($user[$x]==$nombre && $pass[$x]==$password)
+   {
+       $estado=true;
+   }
+   if ($estado)
+   {
+       echo "User Ok";
+   }
+   else 
+   {
+       echo "Not User"
+   }
+   }
+
+
+  ?>
